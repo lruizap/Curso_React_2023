ad428a57003bcfa506a8cfc4101bedc2cf10aa3b
lruizap <lruizap2502@gmail.com>
2023-07-21 13:17:08 +0200

    The game logic is finished

--- ViteReact/projects/02-tic-tac-toe/src/App.jsx
+++ ViteReact/projects/02-tic-tac-toe/src/App.jsx
@@ -22,16 +22,60 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   // Crear el tablero y rellenarlo
   // Estado para guardar el click del jugador y se actualice
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+  // null no hay ganador
+  // false hay empate
+
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  // Poner los valores por defecto 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    // revisamos si hay un empate si no hay
+    // más espacios vacíos en el tablero
+    // Si todas las posiciones del array newBoard son !== a null
+    return newBoard.every((square) => square !== null)
+  }
 
   const updateBoard = (index) => {
-    // No actualizamos posición si ya tiene algo
-    if (board[index]) return
+    // No actualizamos posición si ya tiene algo o tenemos un ganador
+    if (board[index] || winner) return
 
     // Nuevo board para cada vez que se actualice
     const newBoard = [...board]
@@ -47,21 +91,31 @@ function App() {
     // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // empate
+    }
   }
 
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className='game'>
         {
-          board.map((_, index) => {
+          // square, la primera posición
+          board.map((square, index) => {
             return (
               <Square
                 key={index}
                 index={index}
                 updateBoard={updateBoard}
               >
-                {board[index]}
+                {square}
               </Square>
             )
           })
@@ -76,6 +130,31 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'Ganó: '
+                }
+              </h2>
+
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+
+            </div>
+          </section>
+        )
+      }
 
     </main>
   )
